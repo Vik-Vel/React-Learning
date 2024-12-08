c64842e9075775b874ff3cccc96288ef2e2d3d89
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-12-08 22:42:53 +0200

    Modification

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,12 @@
  function Message(){ //PascalCase for React component
     // Call this JavaScript XML
-    return <h1>Hello World</h1>;
+    //return <h1>Hello World</h1>;\
+
+    const name = 'Viksi';
+    if(name)
+        return <h1>Hello {name}</h1>; // If we have a name 
+    return <h1>Hello World</h1>; //else
+    
  }
 
  export default Message;
