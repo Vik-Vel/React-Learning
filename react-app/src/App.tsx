8490c54cf0d0b55b6b18a13bf06cb54daae7f7f2
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-12-08 22:42:41 +0200

    Make a list and inseert it

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,9 @@
- import Message from "./Message"
+ //import Message from "./Message"
 
+ import ListGroup from "./components/ListGroup";
 function App(){
   // return <div><Message></Message></div>
-  return <div><Message /></div> //We can use self closing
+  return <div><ListGroup /></div> //We can use self closing
 }
 
 export default App;
