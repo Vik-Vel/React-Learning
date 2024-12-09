85620a0be05b9588cbc33c1a0df277a10da4ca7e
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-12-09 21:09:06 +0200

    Add more than one element in component
    We cant add more than one element in React component, so we add div or fragment.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,49 @@
+//In React is not allowed to have more than 1 element in 1 component
+//1. With fragments
+
+import { Fragment } from "react/jsx-runtime";
+
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // ctrl + shift + p -- wrap with abbreviature
+    <>
+      {/* == <Fragment> */}
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+    //== </Fragment>
   );
 }
 //1. We rename the class -> className, because the word class is reserved word
 //2. ctrl+d rename all wanted words
 
 export default ListGroup;
+
+//2. With div
+//In React is not allowed to have more than 1 element in 1 component, so we can wrap all in div
+// function ListGroup() {
+//   return (
+
+//     // ctrl + shift + p -- wrap with abbreviature
+//     <div>
+//       <h1>List</h1>
+//       <ul className="list-group">
+//         <li className="list-group-item">An item</li>
+//         <li className="list-group-item">A second item</li>
+//         <li className="list-group-item">A third item</li>
+//         <li className="list-group-item">A fourth item</li>
+//         <li className="list-group-item">And a fifth one</li>
+//       </ul>
+//     </div>
+//   );
+// }
+// //1. We rename the class -> className, because the word class is reserved word
+// //2. ctrl+d rename all wanted words
+
+// export default ListGroup;
