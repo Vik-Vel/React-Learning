e6cac7e17b94976767f1f6535010d5d2a407cab3
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-12-09 21:23:19 +0200

    Update dynamic list
    Make it dynamic with .map function.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -4,17 +4,32 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
+  const items = [
+    "New York",
+    "San Francisco",
+    "Tokyo",
+    "London",
+    "Sofia",
+    "Plovdiv",
+    "Kazanlak",
+  ];
+
   return (
     // ctrl + shift + p -- wrap with abbreviature
+
     <>
       {/* == <Fragment> */}
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+        {/*We remove the old and make it with dynamic with map because we don't have for loop*/}
+        {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">And a fifth one</li> */}
       </ul>
     </>
     //== </Fragment>
